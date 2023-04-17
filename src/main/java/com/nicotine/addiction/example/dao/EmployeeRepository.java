2e4add03dbe09417bb155b9f3fa34aa2f3c85d80
padabed <nikita.padabed@gmail.com>
2023-04-17 19:30:57 +0200

    first commit

--- src/main/java/com/nicotine/addiction/example/dao/EmployeeRepository.java
+++ src/main/java/com/nicotine/addiction/example/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.nicotine.addiction.example.dao;
 
 import com.nicotine.addiction.example.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
